bb28cb8b14dfa2b9c3a88e52f94e68793c8c8d68
rbikash7758 <69745969+rbikash7758@users.noreply.github.com>
2025-02-22 22:39:32 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,28 @@
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "name" {
+  description = "Name tag for the EC2 instance"
+  type        = string
+  default     = "Module-EC2-Instance"
+}
+
+resource "aws_instance" "ec2_instance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  
+  tags = {
+    Name = var.name
+  }
+}
+
+output "instance_id" {
+  value = aws_instance.ec2_instance.id
+}
